b5693cae5c0956b869fac93bfdad162a75c9ab25
olifshitz <omri.lifshtz@gmail.com>
2017-09-03 00:15:57 +0300

    Added results to overheads

--- results/csv_parser.py
+++ results/csv_parser.py
@@ -30,7 +30,7 @@ def parse_dir(output_file, dir):
                 results[threads] = dict()
             results[threads][name] = avg
     with open(output_file, 'w') as csvfile:
-        fieldnames = ['threads', 'BLTree', 'ConcurrentHMAP', 'BST', 'AVL', 'Snap', 'SkipList']
+        fieldnames = ['threads', 'BLTree', 'ConcurrentHMAP', 'BST', 'AVL', 'Snap', 'SkipList', 'SyncTMAP']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
         for name in results:
